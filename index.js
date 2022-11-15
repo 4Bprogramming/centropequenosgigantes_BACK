76a88db912df79ecfdde6cdd028e07f5880bc6f1
Carlos Vazquez Nosetto <78918123+neleon10@users.noreply.github.com>
2022-11-14 21:11:10 -0300

    Creating boiler plate (#2)
    * updating back
    
    * creating back mysql

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-require("dotenv").config()
-const express = require("express")
-const port = process.env.port || 3030
-// require("./db/config") // se trae aca la config de la DB
-
-const server = express()
-server.use(express.json())
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const {port} = process.env;
+// Syncing all the models at once.
+conn.sync({ force: false}).then(() => {
 
 
-//test if this works
-server.get("/", (req, res) => {
-    const content = `
-    <h1>Server con Express</h1>
-    <p>Hola ke asé</p>
-    `
-    res.send(content)
+  server.listen(3001, () => {
+
+    console.log(`Listening at Port `); // eslint-disable-line no-console
+
+
+  });
 });
-
-//Users router
-// server.use("/users", require("./users/usersRoute")) ---> aca se configuran las rutas
-
-//404
-server.use((req, res, next) => {
-    let error = new Error("Resource not found");
-    error.status = 404
-    next(error)
-
-})
-
-//Error handler
-server.use((error, req, res, next) => {
-    if (!error.status) {
-        error.status = 500
-    }
-    res.status(error.status).json({ status: error.status, message: error.message })
-})
-
-
-//listen
-server.listen(port, (err) => {
-    err ? console.log(`Error: ${err}`) : console.log(`Servidor en http://localhost:${port}`)
-})
+ 
