0b8be3161cee7702b3ccfdd2ed7545e180affcb8
Neleon10 <cvn45@hotmail.com>
2022-11-17 00:18:51 -0300

    rutas Posts y validadores

---
+++ src/helpers/validations.js
@@ -0,0 +1,46 @@
+const { check, validationResult } = require("express-validator");
+
+// Express validator se usa para validar datos que viene desde el req.body
+
+const validadorDeDatos = [
+    //validamos nombre
+    check("nombre")
+    .trim()
+    .notEmpty().withMessage("Campo del nombre vacío")
+    .isAlpha('es-ES', { ignore: ' ' }).withMessage("Ingrese solo letras")
+    .isLength({ min: 2, max: 90 }).withMessage("Caractéres mínimos: min 2, max 90"),
+
+    //validamos apellido
+    check("apellido")
+    .trim()
+    .notEmpty().withMessage("Campo del apellido vacío")
+    .isAlpha('es-ES', { ignore: ' ' }).withMessage("Ingrese sólo letras")
+    .isLength({ min: 2, max: 90 }).withMessage("Caractéres mínimos: min 2, max 90"),
+
+    //validamos email
+    check("email")
+    .trim()
+    .notEmpty().withMessage("Este campo no puede estar vacío")
+    .isEmail().withMessage("El email debe ser válido")
+    .normalizeEmail(),
+
+    //validamos password
+    check("password")
+    .trim()
+    .notEmpty().withMessage("Se necesita un password obligatorio")
+    .isLength({ min: 8, max: 15 }).withMessage("El password debe tener entre: min 8, max 15"),
+
+    (req, res, next) => {
+        try {
+            validationResult(req).throw()
+            return next() //si pasa validaciones, sigue hacia el controlador
+        } catch (err) {
+            res.status(400).send({ errors: err.array() })
+        }
+    }
+]
+
+
+
+
+module.exports = {validadorDeDatos}
