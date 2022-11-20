45c5e835c7f3f36779e53b935cbe31eac47adfd6
Carlos Vazquez Nosetto <78918123+neleon10@users.noreply.github.com>
2022-11-20 11:05:40 -0300

    Update Profesional.js

--- src/models/Profesional.js
+++ src/models/Profesional.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
       type:DataTypes.STRING,
       allowNull: false,
     },
-    emailProfesional:{
+    email:{
       type:DataTypes.STRING,
       allowNull: false,
     },
