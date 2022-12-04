b21468ffe9a51a474a3cfed6fb90106376910136
Neleon10 <cvn45@hotmail.com>
2022-12-04 19:24:13 -0300

    tokens agregados al back

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,51 +10,51 @@ const {tokenVerify} = require('../helpers/jwt')
 ///Todas las rutas acá: 
 
 //traer todos los  profesionales
-router.get("/profesionales", profesionales);
+router.get("/profesionales",tokenVerify, profesionales);
 
 // traer profesional por ID
-router.get("/profesionales/:idProfesional",profesionalPorId);
+router.get("/profesionales/:idProfesional",tokenVerify,profesionalPorId);
 
 //buscar todos los usuarios
-router.get ('/usuarios',usuarios)
+router.get ('/usuarios',tokenVerify,usuarios)
 
 // buscar usuario por Email
-router.get ('/usuarios/:email',usuarioPorEmail);
+router.get ('/usuarios/:email',tokenVerify,usuarioPorEmail);
 
 //traer historias clinicas
-router.get('/historiaclinica',traerHistoriaClinica);
+router.get('/historiaclinica',tokenVerify,traerHistoriaClinica);
 
 //traer historia clinica por ID
-router.get('/historiaclinica/:id',traerHistoriaClinicaPorID)
+router.get('/historiaclinica/:id',tokenVerify,traerHistoriaClinicaPorID)
 
 // traer todos los turnos
-router.get('/turnos',traerTurnos);
+router.get('/turnos',tokenVerify,traerTurnos);
 
 // traer turno por ID
-router.get('/turnos/:id',traerTurnoPorID);
+router.get('/turnos/:id',tokenVerify,tokenVerify,traerTurnoPorID);
 
 
 
 //***POSTS*****/
-router.post('/usuarios',validadorDeDatos,crearUsuario);
-router.post('/profesionales',validadorDeDatos,crearProfesional);
-router.post('/turnos',crearTurno);
+router.post('/usuarios',validadorDeDatos,tokenVerify,crearUsuario);
+router.post('/profesionales',validadorDeDatos,tokenVerify,crearProfesional);
+router.post('/turnos',tokenVerify,crearTurno);
 //login
 router.post('/login',login);
 // Crear un Admin Admin
 router.post('/crearadmin',validadorDeAdmin,tokenVerify,crearAdmin);
 
 //Crear historia clinica
-router.post('/historiaclinica',crearHistoriaClinica)
+router.post('/historiaclinica',tokenVerify,crearHistoriaClinica)
 
 
 //***PUT --> (UPDATE)***/
-router.put('/turnos',modificarTurno);
-router.put('/editarprofesional/:idProfesional',sanitizador,editarprofesional)
-router.put('/editarusuario/:email',sanitizador,editarusuario)
+router.put('/turnos',tokenVerify,modificarTurno);
+router.put('/editarprofesional/:idProfesional',sanitizador,tokenVerify,editarprofesional)
+router.put('/editarusuario/:email',sanitizador,tokenVerify,editarusuario)
 
 //dar de baja usuario, admin o profesional --> se modifica el active a False.
-router.put('/altabaja/:email',debajaOdealta);
+router.put('/altabaja/:email',tokenVerify,debajaOdealta);
 
 
 //***SECCION SOLO PARA PASSWORD y NODEMAILER */
