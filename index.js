7e727b6a023d9e240abe82eeba14400165096b8b
Neleon10 <cvn45@hotmail.com>
2022-12-24 15:17:12 +0100

    updatingData

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {port} = process.env;
 // Syncing all the models at once.
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true}).then(() => {
 
 
   server.listen(port, () => {
